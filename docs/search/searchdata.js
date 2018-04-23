ff84dbd5609855e96226c53b1094ed8dec07302a
Pictographer <corbett@pictographer.com>
2018-04-23 13:20:41 -0700

    Tried INPUT +=

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,42 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghilmnoprstuvw",
-  1: "msv",
-  2: "abcelmnpstv",
-  3: "abcdefghilmoprstuvw",
-  4: "abcdfhlmnprstuw",
-  5: "s",
-  6: "acp",
-  7: "bhm",
-  8: "s",
-  9: "d"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "typedefs",
-  6: "enums",
-  7: "defines",
-  8: "groups",
-  9: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Typedefs",
-  6: "Enumerations",
-  7: "Macros",
-  8: "Modules",
-  9: "Pages"
 };
 
